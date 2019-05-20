9467b95fcfa497c637ea4d059c7b41e92a9d01e5
shashi <shashi2030@gmail.com>
2019-05-19 18:54:38 -0700

    ss

--- src/Container/Dispencer.js
+++ src/Container/Dispencer.js
@@ -43,7 +43,7 @@ class Dispencer extends Component{
             <div className="container">
                 <div className="get-money-form container-box">
                         <div className="row">
-                            <label>Enter the Amounts</label>
+                            <label>Enter the Amountss</label>
                             <input type="number" name="amount" onChange={this.handleChange} />
                         </div>
                         <button className="btn" onClick={this.getMoney}>Get Rupees </button>
